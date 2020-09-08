0a66448f4f3fbdbf3a0864556875d0435f45c6a6
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-08 17:41:19 +0200

    Update grocy-chores-card.js
    We do not bind the userId config variable in setup anymore, as the user clicks the tracking button we do the following:
    - Check if there is an userId in the config
    - If not, check if the chore has an next assigned user
    - if not, default to userId = 1

--- grocy-chores-card.js
+++ grocy-chores-card.js
@@ -122,6 +122,8 @@ customElements.whenDefined('card-tools').then(() => {
     _track(choreId, userId){
       if (this.config.user_id != null)
         userId = this.config.user_id;
+      else if (userId == null)
+        userId = 1;
       
       this._hass.callService("grocy", "execute_chore", {
         chore_id: choreId,
