80c1430ecc57ec01af2ce5ba3f8c3da95aabd2d0
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-08 17:36:49 +0200

    Update grocy-chores-card.js

--- grocy-chores-card.js
+++ grocy-chores-card.js
@@ -120,6 +120,9 @@ customElements.whenDefined('card-tools').then(() => {
     } 
 
     _track(choreId, userId){
+      if (this.config.user_id != null)
+        userId = this.config.user_id;
+      
       this._hass.callService("grocy", "execute_chore", {
         chore_id: choreId,
         done_by: userId
@@ -167,7 +170,6 @@ customElements.whenDefined('card-tools').then(() => {
       this.entity = this.config.entity in hass.states ? hass.states[this.config.entity] : null;
 
       this.header = this.config.title == null ? "Chores" : this.config.title;
-      this.userId = this.config.user_id == null ? 1 : this.config.user_id;
 
       this.show_quantity = this.config.show_quantity == null ? null : this.config.show_quantity;
       this.show_days = this.config.show_days == null ? null : this.config.show_days;
