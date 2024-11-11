fa312a0929407cc00ab3c860c61c295e1dc728e3
Frank Hansen <frankhvidbjerg@gmail.com>
2024-11-11 23:11:12 +0100

    Make the add-task-row hidden by default

--- grocy-chores-card.js
+++ grocy-chores-card.js
@@ -387,7 +387,7 @@ class GrocyChoresCard extends LitElement {
 
     _renderAddTask() {
         return html`
-            <div id="add-task-row" class="add-row">
+            <div id="add-task-row" class="add-row hidden-class">
                 <mwc-button @click=${() => this._addTask()}>
                     <ha-icon class="add-icon" icon="mdi:plus"></ha-icon>
                 </mwc-button>
@@ -744,14 +744,14 @@ class GrocyChoresCard extends LitElement {
     }
 
     _toggleAddTask() {
-        const x = this.shadowRoot.getElementById("add-task-row");
-        const icon = this.shadowRoot.getElementById("add-task-icon");
-        if (x.style.display === "none") {
-            x.style.display = "flex";
-            icon.icon = "mdi:chevron-up";
+        const addTaskRow = this.shadowRoot.getElementById("add-task-row");
+        const addTaskIcon = this.shadowRoot.getElementById("add-task-icon");
+        if (addTaskRow.classList.contains('hidden-class')) {
+            addTaskRow.classList.remove('hidden-class');
+            addTaskIcon.icon = "mdi:chevron-up";
         } else {
-            x.style.display = "none";
-            icon.icon = "mdi:chevron-down";
+            addTaskRow.classList.add('hidden-class');
+            addTaskIcon.icon = "mdi:chevron-down";
         }
     }
 
